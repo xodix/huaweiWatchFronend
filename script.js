f7d55dceea6a14dfbfbd63d8421149b87e6ee09e
xodix <kodemode@wp.pl>
2020-09-07 10:51:22 +0200

    even and odd for img

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-window.onload = () => document.getElementById('floaty').addEventListener("click", spy);
+window.onload = () => load();
+
+function load() {
+  document.getElementById('floaty').addEventListener("click", spy);
+  setOrder();
+  window.onresize = () => setOrder();
+}
 
 function spy() {
   const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
@@ -7,15 +13,30 @@ function spy() {
   // Add thresholds
   const scrollPercentage = winScroll / height;
   const smooth = {behavior: 'smooth'};
-  if (scrollPercentage < 0.35) {
+  if (scrollPercentage < 0.33) {
     document.getElementById("2").scrollIntoView(smooth);
-  } else if (scrollPercentage < 0.67) {
+  } else if (scrollPercentage < 0.65) {
     document.getElementById("3").scrollIntoView(smooth);
   } else if (scrollPercentage < 1) {
-    fl.style.transform = "rotateZ(180deg)"
-    document.getElementById("4").scrollIntoView(smooth);
+    fl.style.transform = "rotateZ(180deg)";
+    document.getElementsByTagName("footer")[0].scrollIntoView(smooth);
   } else if (scrollPercentage === 1) {
-    fl.style.transform = "rotateZ(0deg)"
+    fl.style.transform = "rotateZ(0deg)";
     document.getElementById("1").scrollIntoView(smooth);
+  }
+}
+
+function setOrder() {
+  const imgs = document.getElementsByTagName("img");
+  if (window.innerWidth > 1000) {
+    for (let i = 0; i < imgs.length; i++) {
+      if (i % 2 !== 0) {
+        imgs[i].style.order = "2";
+      }
+    }
+  } else {
+    for (let i = 0; i < imgs.length; i++) {
+      imgs[i].style.order = "";
+    }
   }
 }
